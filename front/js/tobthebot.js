c355dd7b32ad2cd22e761a2b254e55ad79cf1032
Jérémy Bouny <jbouny@gmail.com>
2015-01-16 18:17:55 +0100

    Add minimal keyboard handling for control

--- front/js/tobthebot.js
+++ front/js/tobthebot.js
@@ -1,3 +1,39 @@
 $( function() {
-  
+	var socket = io.connect('http://localhost:8080');
+
+	var timeouts = { 37:null, 38:null, 39:null, 40:null } ;
+
+	var LEFT = 37,
+		UP = 38,
+		RIGHT = 39,
+		DOWN = 40 ;
+		
+	var getDisableCallback = function getDisableCallback( key ) {
+		return function() {
+			socket.emit( 'message', { key: key, active: false } ) ;
+			clearTimeout( timeouts[key] );
+			timeouts[key] = null ;
+		} ;
+	}
+
+	$( document ).keydown( function( event ) {
+		var key = event.which;
+		if( key >= LEFT && key <= DOWN ) {
+			var hasTimeout = ( timeouts[key] !== null ) ;
+			if( hasTimeout ) {
+				clearTimeout( timeouts[key] );
+			}
+			else {
+				socket.emit( 'message', { key: key, active: true } ) ;
+			}
+			timeouts[key] = setTimeout( getDisableCallback( key ), 1000 ) ;
+		}
+	});
+
+	$(document).keyup(function( event ){
+		var key = event.which;
+		if( key >= LEFT && key <= DOWN ) {
+			getDisableCallback( key )() ;
+		}
+	});	
 }) ;
