559f97c8e1e97c9afc310792a24353a8fabae35b
Jérémy Bouny <jbouny@gmail.com>
2015-01-17 14:44:10 +0100

    Working controls via keyboard, tested in real conditions

--- server/tobthebot.js
+++ server/tobthebot.js
@@ -1,5 +1,6 @@
 var http = require( 'http' );
 var connect = require( 'connect' );
+var gpio = require( 'onoff' ).Gpio;
 
 // Create server
 var app = connect();
@@ -9,15 +10,114 @@ var server = http.createServer( app );
 
 var io = require( 'socket.io' )( server );
 
+var gpios = [];
+
+var nums = [17, 18, 22, 23]
+for( key in nums ) {
+	console.log( 'Open GPIO ' + nums[key] ) ;
+	gpios[nums[key]] = new gpio( nums[key], 'out' );
+}
+
+function Motor( gpioFront, gpioBack ) {
+	this.gpioFront = gpioFront;
+	this.gpioBack = gpioBack;
+	
+	this.stop = function stop() {
+		gpios[this.gpioFront].writeSync( 0 ) ;
+		gpios[this.gpioBack].writeSync( 0 ) ;
+	}
+	
+	this.front = function front() {
+		gpios[this.gpioFront].writeSync( 1 ) ;
+		gpios[this.gpioBack].writeSync( 0 ) ;
+	}
+	
+	this.back = function back() {
+		gpios[this.gpioFront].writeSync( 0 ) ;
+		gpios[this.gpioBack].writeSync( 1 ) ;
+	}
+}
+
+var motors = {
+	left: new Motor( 17, 18 ),
+	right: new Motor( 22, 23 )
+}
+
+var dir = {
+	front: false,
+	left: false,
+	right: false,
+	back: false
+}
+
 // Callback exemple
 io.sockets.on( 'connection', function ( socket ) {
     console.log( 'A client is connected' );
 	
 	socket.on( 'message', function ( message ) {
-		console.log( 'client: ' + message.key + " " + message.active );
+		if( typeof message.key !== 'undefined' && typeof message.active !== 'undefined' ) {
+			//console.log( 'command: ' + message.key + " " + message.active );
+			
+			switch( message.key )
+			{
+				case 37: // LEFT
+					dir.left = message.active;
+					break;
+				case 38: // FRONT
+					dir.front = message.active;
+					break;
+				case 39: // RIGHT
+					dir.right = message.active;
+					break;
+				case 40: // BACK
+					dir.back = message.active;
+					break;
+			}
+			
+			if( dir.front ) {
+				if( dir.left ) {
+					motors.right.front();
+					motors.left.stop();
+				}
+				else if( dir.right ) {
+					motors.right.stop();
+					motors.left.front();
+				}
+				else {
+					motors.right.front();
+					motors.left.front();
+				}
+			}
+			else if( dir.back ) {
+				if( dir.left ) {
+					motors.right.back();
+					motors.left.stop();
+				}
+				else if( dir.right ) {
+					motors.right.stop();
+					motors.left.back();
+				}
+				else {
+					motors.right.back();
+					motors.left.back();
+				}
+			}
+			else if( dir.left ) {
+				motors.right.front();
+				motors.left.back();
+			}
+			else if( dir.right ) {
+				motors.right.back();
+				motors.left.front();
+			}
+			else {
+				motors.right.stop();
+				motors.left.stop();
+			}
+		}
 	} );
 });
 
 server.listen( 8080, function() {
-	console.log( 'Server listening on port 8080' ) ;
+	console.log( 'Server listening on port 8080' );
 } );
